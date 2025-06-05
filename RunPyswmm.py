f98b74715675475949356965b682db4b416f4c5a
Ava Spangler <145404251+A-Spangler@users.noreply.github.com>
2025-06-05 11:36:43 -0700

    Plotting depth with simulation (not .out files)

--- RunPyswmm.py
+++ RunPyswmm.py
@@ -7,39 +7,46 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 from pyswmm import Simulation, Nodes, Links, Subcatchments, LidControls, LidGroups
-from swmm.toolkit.shared_enum import SubcatchAttribute, NodeAttribute, LinkAttribute
 
-# this runs the simulation I think
-with Simulation(r"/Users/aas6791/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/05 - Research/01 - BSEC Project/SWMM models copy/V19_cleaned_report.inp") as sim:
-    node_object = Nodes(sim)
-    link_object = Links(sim)
-    subcatch_object = Subcatchments(sim)
-    rain_garden = LidControls(sim)["RainGarden"]
+# Initialize Lists for storing data
+time_stamps = []
+J338S_depth = []
+J253S_depth = []
+J366S_depth = []
 
-    # node instantiation
-    J253S = node_object["J253-S"]
+# run the simulation, instantiate BE nodes
+with Simulation(r"/Users/aas6791/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/05 - Research/01 - BSEC Project/SWMM models copy/Inner_Harbor_Model_V19.inp") as sim:
+    J338S = Nodes(sim)['J338-S']
+    J253S = Nodes(sim)['J253-S']
+    J366S = Nodes(sim)['J366-S']
 
-    # Conduit instantiation
-    C6 = link_object["C6"]
+#lets python access sim during run (i think)
+    sim.step_advance(300)
 
-    # subcatchment instantiation
-    SC1 = subcatch_object["S1"]
+# Launch a simulation
+    for ind, step in enumerate(sim):
+        time_stamps.append(sim.current_time)
+        J338S_depth.append(J338S.depth)
+        J253S_depth.append(J253S.depth)
+        J366S_depth.append(J366S.depth)
 
-# output lets you access output without rerunning the file
-from pyswmm import Output, SubcatchSeries, NodeSeries, LinkSeries, SystemSeries
+## Plotting ------------------------------------------------------------------------------------------------------------
 
-with Output('/Users/aas6791/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/05 - Research/01 - BSEC Project/SWMM models copy/V19_cleaned_report.out') as out:
-    print(len(out.subcatchments))
-    print(len(out.nodes))
-    print(len(out.links))
-    print(out.version)
+fig = plt.figure(figsize=(8,4), dpi=200) #Inches Width, Height
+fig.suptitle("Broadway East Node Depths")
+axis_1 = fig.add_subplot(1,1,1)
 
+#plot from sim
+axis_1.plot(time_stamps, J338S_depth, color = 'c', label = 'Chase St. and Rutland St.')
+axis_1.plot(time_stamps, J253S_depth, color = 'm', label = 'Eager St. and Rutland St.')
+axis_1.plot(time_stamps, J366S_depth, color = 'y', label = 'Barnes Street')
 
-    sub_ts = SubcatchSeries(out)['S1'].runoff_rate
-    node_ts = NodeSeries(out)['J1'].invert_depth
-    link_ts = LinkSeries(out)['C2'].flow_rate
-    sys_ts = SystemSeries(out).rainfall
+axis_1.set_ylabel("Depth(ft)")
+#axis_1.get_xticklabels().set_visible(False) # turns off the labels
+axis_1.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d %Hh'))
+axis_1.legend()
 
-    print(sys_ts)
-
+fig.autofmt_xdate()
+plt.tight_layout()
+plt.show()
 
